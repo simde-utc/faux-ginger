57f7d87f20961b0f24179dff96179b1efebe3423
Arthur Puyou <arthur@puyou.fr>
2013-07-05 13:35:42 +0200

    Update users

--- users.php
+++ users.php
@@ -13,7 +13,29 @@ class Users {
             "badge_uid" => "123456AB",
             "expiration_badge" => "2018-02-16"
         ),
-    );    
+        array(
+            "login" => "trecouvr",
+            "nom" => "Recouvreux",
+            "prenom" => "Thomas",
+            "mail" => "thomas.recouvreux@etu.utc.fr",
+            "type" => "etu",
+            "is_adulte" => true,
+            "is_cotisant" => true,
+            "badge_uid" => "ABCDABCD",
+            "expiration_badge" => "2018-01-01"
+        ),
+        array(
+            "login" => "mguffroy",
+            "nom" => "Guffroy",
+            "prenom" => "Matthieu",
+            "mail" => "matthieu.guffroy@etu.utc.fr",
+            "type" => "etu",
+            "is_adulte" => true,
+            "is_cotisant" => true,
+            "badge_uid" => "01234567",
+            "expiration_badge" => "2018-01-01"
+        ),
+    );
 
     public static function getByLogin($login){
         foreach(self::$users as $user){
